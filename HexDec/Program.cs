0432cc6b36b8e208cd998040218204956cb9b118
Jack <113445858+jxkc@users.noreply.github.com>
2022-10-16 09:27:30 +0100

    Converting Decimal updated
    Added stuff to decimal conversion method.

--- HexDec/Program.cs
+++ HexDec/Program.cs
@@ -55,16 +55,13 @@ namespace convert
         //Convert Binary Numbers
         static void ConvertBin()
         {
-            Random qSelect = new Random();
-            int select = r.Next(1, 3);
+            int select = 0;
+            int score = 0;
 
-            string? uAnswer;
-            string? answerDec = "";
+            string? uAnswer = "";
+            string answerDec = "";
             string answerHex = "";
             string questionBin = "";
-
-
-            int score = 0;
 
 
             for(int i = 0; i < 5; i++)
@@ -75,25 +72,9 @@ namespace convert
                 answerHex = Convert.ToString(int.Parse(answerDec), 16).ToUpper();
                 questionBin = Convert.ToString(int.Parse(answerDec), 2);
 
-                if(questionBin.Length > 4)
-                {
-                    questionBin = questionBin.Insert(questionBin.Length - 4, " ");
-                }
-
-                if(questionBin.Length > 8)
-                {
-                    questionBin = questionBin.Insert(questionBin.Length - 9, " ");
-                }
-
-                if (questionBin.Length > 16)
-                {
-                    questionBin = questionBin.Insert(questionBin.Length - 14, " ");
-                }
-
                 switch (select)
                 {
                     case 1:
-                        //conv to dec
                         Console.Write("Please convert {0} to Decimal: ", questionBin);
                         uAnswer = Console.ReadLine();
                         if (uAnswer == answerDec)
@@ -107,7 +88,6 @@ namespace convert
                         }
                         break;
                     case 2:
-                        //conv to hex
                         Console.Write("Please convert {0} to Hexadecimal: ", questionBin);
                         
                         uAnswer = Console.ReadLine();
@@ -131,10 +111,13 @@ namespace convert
             if(score < 2)
             {
                 Console.WriteLine("You fail. Please make sure to revise!");
-            } else if(score == 3 || score == 4)
+            } 
+            
+            else if(score == 3 || score == 4)
             {
                 Console.WriteLine("Well done! Make sure to revise so you can get 100%!");
             }
+
             else
             {
                 Console.WriteLine("Congrats on 100%!");
@@ -143,7 +126,68 @@ namespace convert
 
         static void ConvertDec()
         {
+            int select = 0;
+            int score = 0;
 
+            string? uAnswer = "";
+            string answerBin = "";
+            string answerHex = "";
+            string questionDec = "";
+
+            for(int i = 0; i < 5; i++){
+                select = r.Next(1, 3);
+
+                questionDec = r.Next(1, 65535).ToString();
+
+                answerBin = Convert.ToString(int.Parse(questionDec), 2);
+                answerHex = Convert.ToString(int.Parse(questionDec), 16).ToUpper();
+
+                switch(select){
+                    case 1:
+                        Console.WriteLine("Please convert {0} to Binary", questionDec);
+                        uAnswer = Console.ReadLine();
+
+                        if(uAnswer == answerBin){
+                            Console.WriteLine("Well done! +1 point.");
+                            score++;
+                        } else {
+                            Console.WriteLine("That was not the correct answer.");
+                            Console.WriteLine("The correct answer was: {0}", answerBin);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Please convert {0} to Hexadecimal", questionDec);
+                        
+                        uAnswer = Console.ReadLine();
+                        if(uAnswer == answerHex){
+                            System.Console.WriteLine("Well done! +1 point.");
+                            score++;
+                        } else{
+                            Console.WriteLine("That was not the correct answer.");
+                            Console.WriteLine("The correct answer was: {0}", answerHex);
+                        }
+
+                        break;
+                    default:
+                    System.Console.WriteLine("Error!");
+                        break;
+                }
+            }
+
+            if(score < 2)
+            {
+                Console.WriteLine("You fail. Please make sure to revise!");
+            } 
+            
+            else if(score == 3 || score == 4)
+            {
+                Console.WriteLine("Well done! Make sure to revise so you can get 100%!");
+            }
+
+            else
+            {
+                Console.WriteLine("Congrats on 100%!");
+            }
         }
 
         static void ConvertHex()
